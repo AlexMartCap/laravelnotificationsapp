ad14fda8e61ec1d4ec6497e292212b772f9ec89e
Alexandre <alexmart4@gmail.com>
2015-05-29 17:08:56 +0200

    Stripe i modificacio de plantilla per poder fer login amb plantilla Ace

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,17 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+//Route::get('/', 'WelcomeController@index');
 
-Route::get('home', 'HomeController@index');
+//Route::get('home', 'HomeController@index');
+
+Route::get('/', function(){
+    return View::make('auth/login');
+});
+
+Route::get('home', function(){
+    return View::make('layout');
+});
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
